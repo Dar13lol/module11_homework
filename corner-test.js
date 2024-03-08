5f003a3ff021920a02e4018f577d1e3551a41668
Dar13lol <143339843+Dar13lol@users.noreply.github.com>
2024-03-08 14:14:28 +0500

    Delete corner-test.js

--- corner-test.js
+++
@@ -1,6 +0,0 @@
-import { getMonth } from "function.js";
-describe('getMonth function', () => {
-	it('should return error message for invalid input', () => {
-    expect(getMonth(0)).toBe('Неверный номер месяца');
-    expect(getMonth(13)).toBe('Неверный номер месяца');
-});
